cb17e47933534fb53841eaf31d9315be127e348e
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-04-14 01:27:15 +0900

    Extract with_dsl test helper method

--- test/mcp/dsl_test.rb
+++ test/mcp/dsl_test.rb
@@ -5,15 +5,22 @@ require_relative "../test_helper"
 module MCP
   class DSLTest < MCPTest::TestCase
     def test_server_name
-      mod = Module.new do
-        extend DSL
-
+      server = with_dsl do
         name "test_server"
       end
 
-      server = DSL.build_server(mod)
-
       assert_equal "test_server", server.name
+    end
+
+    private
+
+    def with_dsl(&block)
+      mod = Module.new do
+        extend DSL
+        instance_eval(&block)
+      end
+
+      DSL.build_server(mod)
     end
   end
 end
