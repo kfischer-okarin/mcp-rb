703f904b2f35097de986f1a1ec3149da979e33e9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-05-08 00:00:08 +0900

    Wrap class methods in class << self

--- lib/mcp/dsl.rb
+++ lib/mcp/dsl.rb
@@ -6,15 +6,17 @@ require_relative "dsl/server_builder"
 
 module MCP
   module DSL
-    def self.extended(base)
-      base.instance_variable_set(:@server_builder, ServerBuilder.new)
-      base.extend SingleForwardable
-      base.def_delegators :@server_builder, :name, :version, :resource, :resource_template, :tool
-    end
+    class << self
+      def extended(base)
+        base.instance_variable_set(:@server_builder, ServerBuilder.new)
+        base.extend SingleForwardable
+        base.def_delegators :@server_builder, :name, :version, :resource, :resource_template, :tool
+      end
 
-    def self.build_server(mod)
-      server_builder = mod.instance_variable_get(:@server_builder)
-      server_builder.build
+      def build_server(mod)
+        server_builder = mod.instance_variable_get(:@server_builder)
+        server_builder.build
+      end
     end
   end
 end
